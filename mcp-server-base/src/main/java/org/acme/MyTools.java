168b791c3e2c6aa1572fc6cd9e30d6d685c9814e
Vinicius Ferraz <vinicius.ferraz@gmail.com>
2025-08-20 17:19:43 -0300

    feat: Update MyTools to provide weather alerts for Brazilian regions

--- mcp-server-base/src/main/java/org/acme/MyTools.java
+++ mcp-server-base/src/main/java/org/acme/MyTools.java
@@ -5,19 +5,21 @@ import io.quarkiverse.mcp.server.ToolArg;
 
 public class MyTools {
 
-    @Tool(description = "Get weather alerts for a US state.")
-    String getAlerts(@ToolArg(description = "Two-letter US state code (e.g. CA, NY)") String state) {
-        return "Vai cair um toró";
+    @Tool(description = "Recupera alertas climáticos de regiões Brasileiras")
+    String getAlerts(@ToolArg(description = "Nome de alguma das 5 regiões do Brasil, ex: Nordeste") String state) {
+        if(state.equalsIgnoreCase("Nordeste")) {
+            return "Vai cair um toró";
+        } else if(state.equalsIgnoreCase("Sudeste")) {
+            return "Sem alertas";
+        } else if(state.equalsIgnoreCase("Sul")) {
+            return "Muito Frio";
+        } else if(state.equalsIgnoreCase("Centro-Oeste")) {
+            return "Tempo seco, muita chance de queimadas";
+        } else if(state.equalsIgnoreCase("Norte")) {
+            return "Muita chuva e mosquitos";
+        }
+        return "região desconhecida";
     }
 
-    @Tool(description = "Get weather forecast for a location.")
-    String getForecast(
-            @ToolArg(description = "Latitude of the location") double latitude,
-            @ToolArg(description = "Longitude of the location") double longitude) {
-            return """
-                        Temperature: 33°
-                        Wind: 10 east
-                        """;
-    }
 
 }
